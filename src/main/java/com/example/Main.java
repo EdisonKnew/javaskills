855e4a9e97476b8f1f76b14781ef7ce69204486b
edisonknewgitlab <edisonknew@gmail.com>
2021-06-14 12:04:52 +0200

    Add Route JSon

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -18,6 +18,8 @@ package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -51,6 +53,11 @@ public class Main {
   @RequestMapping("/")
   String index() {
     return "index";
+  }
+
+  @RequestMapping("/datas")
+  JSONObject datas() throws JSONException {
+    return new JSONObject("{'aa':'bb'}");
   }
 
   @RequestMapping("/db")
